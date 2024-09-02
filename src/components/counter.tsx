3e6dcbdc9e6ae7c2b008f7345c4e8b4d38913d4f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-09-02 15:59:31 -0500

    include typescript and functionallity to buttons

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,66 @@
+import { useEffect, useReducer, useState, Dispatch } from "react";
+
+type InitialState = {
+  count: number;
+  draftCount: string | number;
+};
+
+const initialState: InitialState = {
+  count: 0,
+  draftCount: 0
+};
+
+const reducer = (state = initialState, action: any) => {
+  const { count, draftCount } = state;
+
+  if (action.type === 'increment') {
+    const newCount = count + 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'decrement') {
+    const newCount = count - 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'reset') {
+    return { count: 0, draftCount: 0 };
+  }
+
+  if (action.type === 'updateDraftCount') {
+    return { count, draftCount: action.payload };
+  }
+
+  if (action.type === 'updateCountFromDraft') {
+    return { count: Number(draftCount), draftCount };
+  }
+
+  return state;
+};
+
 const Counter = () => {
+  const [{ count, draftCount }, dispatch] = useReducer(reducer, initialState);
+  const [valueToAdd, setValueToAdd] = useState<number>(count);
+
+  useEffect(() => {
+    setValueToAdd(count)
+  }, [count]);
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>➖ Decrement</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>🔁 Reset</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: 'updateCountFromDraft' });
+        }}>
+          <input type="number" value={valueToAdd} onChange={(e) => dispatch({ type: 'updateDraftCount', payload: e.target.value })} />
           <button type="submit">Update Counter</button>
         </form>
       </div>
